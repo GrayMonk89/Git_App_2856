8693cf917aed10ddde7e6aa86db70f7833a80f0e
GrayMonk <graymonk89@yandex.ru>
2023-06-10 22:06:34 +0300

    Ломовое решение.

--- app/src/main/java/ru/graymonk/poplib/gitapp/MainActivity.kt
+++ app/src/main/java/ru/graymonk/poplib/gitapp/MainActivity.kt
@@ -7,9 +7,33 @@ import ru.graymonk.poplib.gitapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val counter = mutableListOf(0, 0, 0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initCounters()
+
+        setOnClickListener()
+    }
+
+    private fun initCounters() {
+        binding.mainActivityTextViewCounterOne.text = counter[0].toString()
+        binding.mainActivityTextViewCounterTwo.text = counter[1].toString()
+        binding.mainActivityTextViewCounterThree.text = counter[2].toString()
+    }
+
+    private fun setOnClickListener() {
+        binding.mainActivityButtonCounterOne.setOnClickListener {
+            binding.mainActivityTextViewCounterOne.text = (++counter[0]).toString()
+        }
+        binding.mainActivityButtonCounterTwo.setOnClickListener {
+            binding.mainActivityTextViewCounterTwo.text = (++counter[1]).toString()
+        }
+        binding.mainActivityButtonCounterThree.setOnClickListener {
+            binding.mainActivityTextViewCounterThree.text = (++counter[2]).toString()
+        }
     }
 }
